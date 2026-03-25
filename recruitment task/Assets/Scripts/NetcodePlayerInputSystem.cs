9cb97075373b82163c8baef27ecd7b949c6bfa0b
Inari69 <rafalpajak2003@gmail.com>
2026-03-25 19:40:51 +0100

    player input work in progress

---
+++ recruitment task/Assets/Scripts/NetcodePlayerInputSystem.cs
@@ -0,0 +1,31 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.NetCode;
+
+[UpdateInGroup(typeof(GhostInputSystemGroup))]
+partial struct NetcodePlayerInputSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<NetworkStreamInGame>();
+        state.RequireForUpdate<NetcodePlayerInput>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        float2 move = PlayerInputBridge.Move;
+        foreach (RefRW<NetcodePlayerInput> netcodePlayerInput in SystemAPI.Query<RefRW<NetcodePlayerInput>>().WithAll<GhostOwnerIsLocal>())
+        {
+            netcodePlayerInput.ValueRW.inputVector = move;
+        }
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+        
+    }
+}
---
+++ recruitment task/Assets/Scripts/PlayerInputBridge.cs
@@ -0,0 +1,32 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputBridge : MonoBehaviour
+{
+    public static float2 Move;
+
+    private InputSystem_Actions _inputActions;
+
+    private void Awake()
+    {
+        _inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
+    }
+
+    void Update()
+    {
+        Vector2 move = _inputActions.Player.Move.ReadValue<Vector2>();
+        Move = move;
+    }
+}
