a71dd63e97a23a2b55e869d1b8e7080ce986192b
Inari69 <rafalpajak2003@gmail.com>
2026-03-28 19:25:13 +0100

    Update ChatServerSystem.cs

--- recruitment task/Assets/Scripts/ChatServerSystem.cs
+++ recruitment task/Assets/Scripts/ChatServerSystem.cs
@@ -1,50 +1,54 @@
 using Unity.Entities;
 using Unity.NetCode;
+using Unity.Collections;
 
 [WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
 public partial struct ChatServerSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+
         foreach (var (rpc, entity) in
                  SystemAPI.Query<RefRO<ChatMessageRpc>>()
                      .WithAll<ReceiveRpcCommandRequest>()
                      .WithEntityAccess())
         {
-            var em = state.EntityManager;
-
             var msg = rpc.ValueRO;
             
             if (msg.TargetID == -1)
             {
-                foreach (var connection in SystemAPI.Query<RefRO<NetworkId>>().WithEntityAccess())
+                foreach (var (netId, connEntity) in
+                         SystemAPI.Query<RefRO<NetworkId>>().WithEntityAccess())
                 {
-                    Entity rpcEntity = em.CreateEntity();
+                    Entity rpcEntity = ecb.CreateEntity();
 
-                    em.AddComponentData(rpcEntity, new ChatMessageRpc
+                    ecb.AddComponent(rpcEntity, new ChatMessageRpc
                     {
                         SenderNickname = msg.SenderNickname,
                         TargetID = msg.TargetID,
                         Message = msg.Message
                     });
 
-                    em.AddComponentData(rpcEntity, new SendRpcCommandRequest
+                    ecb.AddComponent(rpcEntity, new SendRpcCommandRequest
                     {
-                        TargetConnection = connection.Item2
+                        TargetConnection = connEntity
                     });
                 }
             }
+
             else
             {
-                foreach (var (id, connEntity) in SystemAPI.Query<RefRO<NetworkId>>().WithEntityAccess())
+                foreach (var (netId, connEntity) in
+                         SystemAPI.Query<RefRO<NetworkId>>().WithEntityAccess())
                 {
-                    if (id.ValueRO.Value == msg.TargetID)
+                    if (netId.ValueRO.Value == msg.TargetID)
                     {
-                        Entity rpcEntity = em.CreateEntity();
+                        Entity rpcEntity = ecb.CreateEntity();
 
-                        em.AddComponentData(rpcEntity, msg);
+                        ecb.AddComponent(rpcEntity, msg);
 
-                        em.AddComponentData(rpcEntity, new SendRpcCommandRequest
+                        ecb.AddComponent(rpcEntity, new SendRpcCommandRequest
                         {
                             TargetConnection = connEntity
                         });
@@ -54,7 +58,10 @@ public partial struct ChatServerSystem : ISystem
                 }
             }
 
-            em.DestroyEntity(entity);
+            ecb.DestroyEntity(entity);
         }
+
+        ecb.Playback(state.EntityManager);
+        ecb.Dispose();
     }
 }
